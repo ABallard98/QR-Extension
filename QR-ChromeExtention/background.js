bd91ffeb439b199f66ff124c04e13d399af75b5e
ABallard98 <905438@swansea.ac.uk>
2018-09-23 17:50:04 +0100

    fixed it

--- QR-ChromeExtention/background.js
+++ QR-ChromeExtention/background.js
@@ -12,28 +12,25 @@ This function will create a QR code and display in on the window
 of the extension.g
 */
 function displayURL(){
-	chrome.tabs.getSelected(null, function (tab) {
-	var url = new URL(tab.url)
-	var domain = url.hostname
-	var text = document.getElementById("testText"); //displaying url for testing
-	text.innerHTML = url;
 	
-	//generating QR code
-	new QRCode(document.getElementById("qrcode"), {
-		text: url,
-		width: 128,
-		height: 128,
-		colorDark : "#000000",
-		colorLight : "#ffffff",
-		correctLevel : QRCode.CorrectLevel.H
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		var currTab = tabs[0];
+		if (currTab) { 
+			var url = currTab.url;
+							
+			//generating QR code
+			new QRCode(document.getElementById("qrcode"), {
+				text: url,
+				width: 128,
+				height: 128,
+				colorDark : "#000000",
+				colorLight : "#ffffff",
+				correctLevel : QRCode.CorrectLevel.H
+			});
+		} //end of if statement
 	});
-});
 }
 
-function testTextFunc(){
-	var text = document.getElementById("testText");
-	text.innerHTML = "hello world";
-}
 
 
 
